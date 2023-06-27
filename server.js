14fb3cf9e3338f40bca8a2312753876dc9194d0f
C12 <mcpartlandpatrick@gmail.com>
2023-06-26 22:01:01 -0400

    Updated server.js, created all.handlebars in views, added test.js in controllers with get route for all.js

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -20,14 +31,13 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
-
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
