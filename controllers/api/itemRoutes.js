51b382993508c01739d723df1c71ec00b8256e7e
rtm713 <rtm713@gmail.com>
2023-06-29 16:18:01 -0500

    added login, signup, and connected items to items page

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -16,7 +16,8 @@ router.get('/', async (req, res) => {
         
         const items = itemsData.map((items) => items.get({ plain: true }));
 
-        res.status(200).json(items)
+        res.render('item', { items });
+
     } catch(err) {
         res.status(400).json(err);
     }
