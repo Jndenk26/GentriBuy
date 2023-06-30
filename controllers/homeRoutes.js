9fce89e6f2ba441acdb5015cbbb588ec7029425a
Lendo <wesreedbusiness@gmail.com>
2023-06-29 20:27:10 -0500

    signUp js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -32,11 +32,16 @@ router.get('/home', async (req, res) => {
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/');
+      res.redirect('/home');
       return;
     }
   
     res.render('login');
   });
 
+  router.get('/signup', (req, res) => {
+
+    res.render('signup');
+  });
+  
   module.exports = router;
