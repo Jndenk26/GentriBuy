06af317b4df4642ebfeb81d8e4a8cbf5d1051fc2
Lendo <wesreedbusiness@gmail.com>
2023-07-06 18:18:25 -0500

    logout

---
+++ public/js/logout.js
@@ -0,0 +1,14 @@
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      throw err
+    }
+  };
+  
+  document.querySelector('#logoutButton').addEventListener('click', logout);
